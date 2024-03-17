a14630c5e88132690af68ab6cffb5019a1e38bcd
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-17 23:48:21 +0330

    Added interfaces for services and dbcontext

---
+++ src/NadinSoftTask.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using NadinSoftTask.Core.Models;
+using System.Linq.Expressions;
+
+namespace NadinSoftTask.Core.Interfaces;
+
+public interface IGenericRepository<T> where T : EntityBase
+{
+    Task<int> CreateAsync(T entity);
+    Task<T> GetByIdAsync(int id);
+    Task<List<T>> ListAsync(int? skip, int? take, params Expression<Func<T, object>>[] includes);
+    Task<List<T>> ListAsync(Expression<Func<T, bool>>[] filters, int? skip, int? take, params Expression<Func<T, object>>[] includes);
+    Task UpdateAsync(T entity);
+    Task Delete(int id);
+    Task SaveChangesAsync();
+}
---
+++ src/NadinSoftTask.Core/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using NadinSoftTask.Core.Dtos.Product;
+
+namespace NadinSoftTask.Core.Interfaces;
+
+public interface IProductService
+{
+    Task<int> CreateProductAsync(ProductCreate productCreate);
+    Task<List<ProductGet>> ListProductsAsync(ProductFilter productFilter);
+    Task<ProductGet> GetByIdAsync(int id);
+    Task UpdateAsync(ProductUpdate productUpdate);
+    Task DeleteAsync(int id);
+}
