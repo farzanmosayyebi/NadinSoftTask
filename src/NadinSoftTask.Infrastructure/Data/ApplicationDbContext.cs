05bfc492538c8f54262d75419afb9231160b296a
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-17 23:49:40 +0330

    dbcontext

---
+++ src/NadinSoftTask.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NadinSoftTask.Core.Models;
+
+namespace NadinSoftTask.Infrastructure.Data;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+{
+    public DbSet<Product> Products { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+    }
+}
