a698899d089b0c753f08a01771684fd637442210
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-18 18:00:04 +0330

    feat: AutoMapper configuration

---
+++ src/NadinSoftTask.Infrastructure/ExternalServices/DtoEntityMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using NadinSoftTask.Core.Dtos.Product;
+using NadinSoftTask.Core.Models;
+
+namespace NadinSoftTask.Infrastructure.ExternalServices;
+
+public class DtoEntityMapperProfile : Profile
+{
+    public DtoEntityMapperProfile()
+    {
+        CreateMap<ProductUpdate, Product>();
+        CreateMap<ProductCreate, Product>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+        CreateMap<Product, ProductGet>()
+            .ForMember(dest => dest.CreatorId, opt => opt.MapFrom(src => src.Creator.Id));
+        CreateMap<Product, Product>();
+    }
+}
