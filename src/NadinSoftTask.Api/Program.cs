7e1e09615ed2e1eebb808ddf51bbfd20e64ec210
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-19 01:20:09 +0330

    feat: configuration

--- src/NadinSoftTask.Api/Program.cs
+++ src/NadinSoftTask.Api/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using NadinSoftTask.Application;
 using NadinSoftTask.Infrastructure.Data;
+
 
 namespace NadinSoftTask.Api;
 
@@ -10,10 +12,16 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        DependencyInjectionConfiguration.RegisterServices(builder.Services);
 
-        //builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        //    options.UseSqlServer();
+        string connectionString = builder.Configuration.GetConnectionString("DataBase");
 
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString,
+                opt => opt.UseDateOnlyTimeOnly())
+            );
+
+        
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
@@ -27,6 +35,14 @@ public class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+
+        using (var serviceScope = app.Services.CreateScope())
+        {
+            var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            dbContext.Database.EnsureCreated();
+            dbContext.Database.Migrate();
+        }
+
 
         app.UseHttpsRedirection();
 
