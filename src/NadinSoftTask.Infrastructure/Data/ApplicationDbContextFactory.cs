b5744a8b02571abc653ff4a39d072cd5e3a896bd
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-19 12:35:28 +0330

    feat: Added DbContext factory

---
+++ src/NadinSoftTask.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace NadinSoftTask.Infrastructure.Data;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        string connectionString = "Server=(localdb)\\mssqllocaldb;Database=ProductCatalogue;Integrated Security=True;";
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        optionsBuilder.UseSqlServer(connectionString, opt => opt.UseDateOnlyTimeOnly());
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
