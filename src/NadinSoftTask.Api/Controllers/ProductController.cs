a4cf652aa9a52b44bc430dd6bccd4117c7d11040
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-21 01:19:40 +0330

    feat: added [Authorize] attributes

--- src/NadinSoftTask.Api/Controllers/ProductController.cs
+++ src/NadinSoftTask.Api/Controllers/ProductController.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using NadinSoftTask.Core.Dtos.Product;
 using NadinSoftTask.Core.Interfaces;
+using NadinSoftTask.Core.Models;
+using System.Security.Claims;
 
 namespace NadinSoftTask.Api.Controllers;
 
@@ -8,45 +14,50 @@ namespace NadinSoftTask.Api.Controllers;
 [Route("[controller]")]
 public class ProductController : ControllerBase
 {
+    private readonly UserManager<ApplicationUser> _userManager;
     private readonly IProductService _productService;
 
-    public ProductController(IProductService productService)
+    public ProductController(IProductService productService, UserManager<ApplicationUser> userManager)
     {
         _productService = productService;
+        _userManager = userManager;
     }
 
     [HttpGet]
     [Route("Get/{id}")]
     public async Task<IActionResult> GetProductById(int id)
     {
-        ProductGet product = await _productService.GetProductByIdAsync(id);
+        ProductGetDto product = await _productService.GetProductByIdAsync(id);
         return Ok(product);
     }
 
     [HttpGet]
     [Route("Get")]
-    public async Task<IActionResult> GetProducts(ProductFilter productFilter)
+    public async Task<IActionResult> GetProducts([FromQuery] ProductFilterDto productFilter)
     {
-        List<ProductGet> products = await _productService.ListProductsAsync(productFilter);
+        List<ProductGetDto> products = await _productService.ListProductsAsync(productFilter);
         return Ok(products);
     }
 
+    [Authorize]
     [HttpPost]
     [Route("Create")]
-    public async Task<IActionResult> CreateProduct(ProductCreate productCreate)
+    public async Task<IActionResult> CreateProduct(ProductCreateDto productCreate)
     {
         int id = await _productService.CreateProductAsync(productCreate);
         return Ok(id);
     }
 
+    [Authorize]
     [HttpPut]
     [Route("Update")]
-    public async Task<IActionResult> UpdateProduct(ProductUpdate productUpdate)
+    public async Task<IActionResult> UpdateProduct(ProductUpdateDto productUpdate)
     {
-        ProductGet updatedProduct = await _productService.UpdateProductAsync(productUpdate);
+        ProductGetDto updatedProduct = await _productService.UpdateProductAsync(productUpdate);
         return Ok(updatedProduct);
     }
 
+    [Authorize]
     [HttpDelete]
     [Route("Delete")]
     public async Task<IActionResult> DeleteProduct(int id)
