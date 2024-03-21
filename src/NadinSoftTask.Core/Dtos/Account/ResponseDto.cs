4bc171292f01db974e4e29a9e11d5bc260233083
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-21 14:37:24 +0330

    style: renamed to Account

---
+++ src/NadinSoftTask.Core/Dtos/Account/ResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace NadinSoftTask.Core.Dtos.Security;
+
+public record ResponseDto
+{
+    public string Status { get; set; } = default!;
+    public string Message { get; set; } = default!;
+}
---
+++ src/NadinSoftTask.Core/Dtos/Account/UserRegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NadinSoftTask.Core.Dtos.Security;
+
+public record UserRegisterDto
+{
+    [Required]
+    public string UserName { get; set; } = default!;
+    [Required]
+    public string Password { get; set; } = default!;
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = default!;
+}
