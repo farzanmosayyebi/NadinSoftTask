6f1f75e4c02069f0d77f36af022f052f9a3fb9cf
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-22 19:15:12 +0330

    refactor: moved implementation to application layer

--- src/NadinSoftTask.Api/Controllers/AccountController.cs
+++ src/NadinSoftTask.Api/Controllers/AccountController.cs
@@ -1,14 +1,14 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using NadinSoftTask.Core.Dtos.Security;
-using NadinSoftTask.Core.Models;
-using System.Data;
+﻿using System.Data;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+using NadinSoftTask.Core.Dtos.Security;
+using NadinSoftTask.Core.Interfaces;
+using NadinSoftTask.Core.Models;
 
 namespace NadinSoftTask.Api.Controllers;
 
@@ -16,41 +16,23 @@ namespace NadinSoftTask.Api.Controllers;
 [Route("[controller]")]
 public class AccountController : ControllerBase
 {
-    private readonly UserManager<ApplicationUser> _userManager;
-    private readonly RoleManager<ApplicationRole> _roleManager;
-    private readonly SignInManager<ApplicationUser> _signInManager;
     private readonly IConfiguration _configuration;
-    private readonly IMapper _mapper;
+    private readonly IAccountService _accountService;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public AccountController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager,
-        IConfiguration configuration, IMapper mapper, SignInManager<ApplicationUser> signInManager)
+    public AccountController(UserManager<ApplicationUser> userManager, IConfiguration configuration,
+                            IAccountService accountService)
     {
         _userManager = userManager;
-        _roleManager = roleManager;
         _configuration = configuration;
-        _mapper = mapper;
-        _signInManager = signInManager;
+        _accountService = accountService;
     }
 
     [HttpPost]
     [Route("Login")]
-    public async Task<IActionResult> Login([FromQuery] UserLoginDto loginCredentials)
+    public async Task<IActionResult> Login([FromQuery] UserLoginDto loginDto)
     {
-        var user = await _userManager.FindByNameAsync(loginCredentials.UserName);
-        if (user == null)
-            return NotFound(new ResponseDto { Status = "Error", Message = "No user with the entered username was found" });
-        if (!await _userManager.CheckPasswordAsync(user, loginCredentials.Password))
-            return Unauthorized();
-
-        var userRoles = await _userManager.GetRolesAsync(user);
-        var authClaims = new List<Claim>
-        {
-            new Claim(ClaimTypes.Name, user.UserName),
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-        };
-
-        authClaims.AddRange(
-            userRoles.Select(r => new Claim(ClaimTypes.Role, r)));
+        var authClaims = await _accountService.Login(loginDto);
 
         var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
 
@@ -71,26 +53,10 @@ public class AccountController : ControllerBase
 
     [HttpPost]
     [Route("SignUp")]
-    public async Task<IActionResult> SignUp([FromQuery] UserRegisterDto registerInfo)
+    public async Task<IActionResult> SignUp([FromQuery] UserRegisterDto registerDto)
     {
-        var user = await _userManager.FindByNameAsync(registerInfo.UserName);
-
-        if (user != null)
-            return StatusCode(StatusCodes.Status400BadRequest, 
-                new ResponseDto { Status = "Error", Message = "A user with entered username already exists" });
-
-        ApplicationUser newUser = new ApplicationUser
-        {
-            UserName = registerInfo.UserName,
-            Email = registerInfo.Email,
-        };
-
-        var result = await _userManager.CreateAsync(newUser, registerInfo.Password);
+        await _accountService.Register(registerDto);
         
-        if (!result.Succeeded)
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                new ResponseDto { Status = "Error", Message = "User creation failed please try again." });
-
         return Ok();
     }
 }
