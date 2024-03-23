971549b3752d964fd31871cab5e91504f3702d64
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-23 19:11:43 +0330

    feat: implemented custom exceptions

---
+++ src/NadinSoftTask.Core/Exceptions/ItemNotFoundException.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+
+namespace NadinSoftTask.Core.Exceptions;
+
+[Serializable]
+public class ItemNotFoundException : Exception
+{
+    public readonly int? ItemId;
+    public readonly string? ItemType;
+    public ItemNotFoundException(int itemId, string itemType)
+    {
+        ItemId = itemId;
+        ItemType = itemType;
+    }
+
+    public ItemNotFoundException(string? message) : base(message)
+    {
+    }
+
+    public ItemNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected ItemNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
