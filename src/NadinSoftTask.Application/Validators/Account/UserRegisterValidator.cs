753dc514ab004b7423058ade6df67e0ad3e7993e
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-23 17:47:24 +0330

    feat: added validation rules using FluentValidation

---
+++ src/NadinSoftTask.Application/Validators/Account/UserRegisterValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using NadinSoftTask.Core.Dtos.Security;
+
+namespace NadinSoftTask.Application.Validators.Account;
+
+public class UserRegisterValidator : AbstractValidator<UserRegisterDto>
+{
+    public UserRegisterValidator()
+    {
+        RuleFor(input => input.UserName).NotEmpty()
+            .MaximumLength(30)
+            .Matches("^[A-Za-z][A-Za-z0-9_]{7,29}$");
+
+        RuleFor(input => input.Password).NotEmpty().Matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}");
+        
+        RuleFor(input => input.Email).NotEmpty().EmailAddress();
+    }
+}
---
+++ src/NadinSoftTask.Application/Validators/Product/ProductCreateValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using NadinSoftTask.Core.Dtos.Product;
+
+namespace NadinSoftTask.Application.Validators.Product;
+
+public class ProductCreateValidator : AbstractValidator<ProductCreateDto>
+{
+    public ProductCreateValidator()
+    {
+        RuleFor(input => input.ProduceDate).NotEmpty()
+            .Must(input => input.CompareTo(DateOnly.FromDateTime(DateTime.Today.Date)) <= 0);
+
+        RuleFor(input => input.ManufacturerEmail).NotEmpty().EmailAddress();
+
+        RuleFor(input => input.ManufacturerPhone).NotEmpty()
+            .Matches("^0\\d{2}-\\d{8}$"); // example: 021-12345678
+
+        RuleFor(input => input.Name).NotEmpty().MaximumLength(255);
+    }
+}
