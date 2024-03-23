3a6ed6ed4d81840aac83c372e48cf8926cde63bc
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-23 21:56:07 +0330

    feat: user info dto

---
+++ src/NadinSoftTask.Core/Dtos/Account/UserInfoDto.cs
@@ -0,0 +1,11 @@
+﻿using NadinSoftTask.Core.Dtos.Product;
+
+namespace NadinSoftTask.Core.Dtos.Account;
+
+public record UserInfoDto(
+    Guid Id,
+    string UserName,
+    string Email,
+    List<ProductGetDto> CreatedProducts);
+
+
